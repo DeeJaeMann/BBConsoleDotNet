e98610f2a6fb9335314e229c7b03d4a0819a3074
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-08-27 15:47:46 -0400

    Added frameworks
    Created Classes claslib
    Created Classes.Tests NUnit testing libraray

---
+++ Classes.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+namespace BBConsoleDotNet.Classes.Tests
+{
+
+
+    public class Tests
+    {
+        Store block_buster; 
+        [SetUp]
+        public void Setup()
+        {
+            block_buster = new("Block Buster");
+            block_buster.LoadData("customers");
+            block_buster.LoadData("inventory");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Classes/Store.cs
@@ -0,0 +1,16 @@
+﻿namespace BBConsoleDotNet.Classes
+{
+    public class Store
+    {
+        string _storeName;
+        public Store(string storeName)
+        {
+            _storeName = storeName;
+        }
+
+        public void LoadData(string fileName)
+        {
+
+        }
+    }
+}
